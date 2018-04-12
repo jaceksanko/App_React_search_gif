6a9792a8da9dc8422614a17147a469c9ba4e0aa9
Jacek Sanko <jacek.sanko@gmail.com>
2018-04-12 15:10:36 +0200

    Change callback for promise in App.js

--- components/App.js
+++ components/App.js
@@ -6,6 +6,35 @@ App = React.createClass({
             gif: {}
         }
     },
+    getGif: function(searchingText, callback) {
+        function getUrl() {
+            return new Promise(
+                function(resolve, reject) {
+                    const url = 'http://api.giphy.com' + '/v1/gifs/random?api_key=' + 'V5fnADF5p9n6tEEx76OiM43fAEdmdnF7' + '&tag=' + searchingText; 
+                    const xhr = new XMLHttpRequest();
+                    xhr.open('GET', url);
+                    xhr.onload = function() {
+                        if (xhr.status === 200) {
+                            resolve(xhr.responseText);
+                        }
+                    };
+                    xhr.send();
+                }
+            )
+        }
+        getUrl()
+        .then(resp => {
+            const data = JSON.parse(resp).data;
+            let gif = { 
+            url: data.fixed_width_downsampled_url, // nie rozumiem co to jest "fixed_width_downsampled_url"
+            sourceUrl: data.url
+            };
+            callback(gif);
+        })
+    },
+
+
+/* 
     getGif: function(searchingText, callback) { 
         var url = 'http://api.giphy.com' + '/v1/gifs/random?api_key=' + 'V5fnADF5p9n6tEEx76OiM43fAEdmdnF7' + '&tag=' + searchingText; 
         var xhr = new XMLHttpRequest();
@@ -21,7 +50,7 @@ App = React.createClass({
             }
         };
         xhr.send();
-    },
+    }, */
 
     handleSearch: function(searchingText) {
         this.setState({
